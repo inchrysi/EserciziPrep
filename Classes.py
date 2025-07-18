28eb0254e81c7e473eb3a50bd721512bd8420c68
inchrysi <chriselleannguillermo@gmail.com>
2025-07-18 09:46:13 +0200

    Create Classes.py
    Trying out object programming

---
+++ Classes.py
@@ -0,0 +1,55 @@
+class Gene:
+    def __init__(self, name, length):
+        self.setNome(name)
+        self.length = length
+        
+    def setNome(self, newName):
+        newName = newName
+        self.__name = newName
+        
+    def describe(self):
+        print('Gene ' + self.__name + ' is long ' + str(self.length) + ' bp')
+
+class Sample:
+    def __init__(self, name, expressedGenes):
+        self.setName(name)
+        self.expressedGenes = expressedGenes
+        
+    def addGene(self, geneName):
+        self.expressedGenes.append(geneName)
+    
+    def showGenes(self):
+        print(self.expressedGenes)
+    
+    def setName(self, newName):
+        newName = newName
+        self.__name = newName
+
+class DNASequence:
+    def __init__(self, sequence):
+        self.sequence = sequence
+    
+    def GCcontent(self):
+        seq = self.sequence
+        count = 0
+        
+        for x in seq:
+            if x == 'G' or x == 'C':
+                count+=1
+        
+        GC = count * 100 / len(seq)
+        
+        return GC
+
+#DA RIVEDERE
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    class Patient:
+        def __init__(self, diagnosis):
+            self.diagnosis = diagnosis
+        
+        def summary(self):
+            print(Person, '-', self.diagnosis)
